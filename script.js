a969dabccf8101afa090e886fc5e24ead9f7616f
eutyuganov-droid <eutyuganov@yandex.ru>
2026-04-23 10:21:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const messages = [
-    "Они видят тебя...",
-    "Не оборачивайся...",
-    "Кто-то рядом...",
-    "Сигнал нестабилен...",
-    "Ты следующий...",
-    "Охота началась...",
-    "Беги..."
-];
 
-let i = 0;
-setInterval(() => {
-    document.getElementById("status").innerText =
-        messages[Math.floor(Math.random() * messages.length)];
-}, 2000);
